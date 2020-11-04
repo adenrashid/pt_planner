8b7b0d9e72ff413bb43de35d3dea94e2b447e9d2
aden <adenrashid@hotmail.com>
2020-11-04 17:58:41 +1100

    hw changes

--- trains.js
+++ trains.js
@@ -6,53 +6,76 @@ var trains = [
 
 var origin = prompt("Enter origin station", origin);
 var destination = prompt("Enter destination station", destination);
+getRoute();
 
-console.log(`Origin: ${origin}`);
-console.log(`Destination: ${destination}`);
+function getRoute() {
+    const first_line = trains.filter(line => line.stops.includes(origin))[0];
+    const second_line = trains.filter(line => line.stops.includes(destination))[0];
+    const isValidInput = !first_line || !second_line;
+    const isSingleLine = first_line === second_line;
 
-for (let i = 0; i < trains.length; i++) {
-    if (trains[i].stops.includes(origin) && trains[i].stops.includes(destination)) {
-        sameLine(trains[i])
-    } 
-};
-
-var first_line = trains.filter(line => line.stops.includes(origin))[0]
-var second_line = trains.filter(line => line.stops.includes(destination))[0]
-if (first_line.name != second_line.name) {
-    multipleLines(first_line, second_line)
-};
+    if (isValidInput) {
+        alert("Enter a valid destination");
+        origin = prompt("Enter origin station", origin);
+        destination = prompt("Enter destination station", destination);
+        getRoute();
+    } else if (isSingleLine) {
+        sameLine(first_line)
+    } else {
+        multipleLines(first_line, second_line)
+    };
+}
 
 function sameLine(line) {
-    console.log(`Stops: ${Math.abs(line.stops.indexOf(origin) - line.stops.indexOf(destination))}`);
-    if (line.stops.indexOf(origin) < line.stops.indexOf(destination)) {
-        var journey = line.stops.slice(line.stops.indexOf(origin), line.stops.indexOf(destination)+1);
-        var final_journey_array = [];
-        for (let i = 0; i < journey.length; i++) {
-            final_journey_array.push(journey[i])
-        };
-        console.log(final_journey_array.join(' ---> '));
+    const stops = Math.abs(line.stops.indexOf(origin) - line.stops.indexOf(destination));
+    const trainRunningForwards = line.stops.indexOf(origin) < line.stops.indexOf(destination);
+
+    if (trainRunningForwards) {
+        let journey = line.stops.slice(line.stops.indexOf(origin), line.stops.indexOf(destination)+1);
+        getStops(journey);
     } else {
-        var journey = line.stops.reverse().slice(line.stops.indexOf(origin), line.stops.indexOf(destination)+1);
-        var final_journey_array = [];
-        for (let i = 0; i < journey.length; i++) {
-            final_journey_array.push(journey[i])
-        };
-        console.log(final_journey_array.join('--->'));
+        let journey = line.stops.reverse().slice(line.stops.indexOf(origin), line.stops.indexOf(destination)+1);
+        getStops(journey);
     }
+    renderRoute(origin, destination, stops);
 };
 
 function multipleLines(line1, line2) {
-    if (line1.stops.indexOf(origin) < line1.stops.indexOf('Richmond')) {
+    const originBeforeRichmond = line1.stops.indexOf(origin) < line1.stops.indexOf('Richmond');
+    const destinationBeforeRichmond = line2.stops.indexOf(destination) < line2.stops.indexOf('Richmond');
+
+    if (originBeforeRichmond) {
         var line1_journey = line1.stops.slice(line1.stops.indexOf(origin), line1.stops.indexOf('Richmond')+1)
     } else {
         var line1_journey = line1.stops.reverse().slice(line1.stops.indexOf(origin), line1.stops.indexOf('Richmond')+1)
     }
-    if (line2.stops.indexOf(destination) < line2.stops.indexOf('Richmond')) {
+    if (destinationBeforeRichmond) {
         var line2_journey = line2.stops.slice(line2.stops.indexOf(destination), line2.stops.indexOf('Richmond')+1)
     } else {
         var line2_journey = line2.stops.reverse().slice(line2.stops.indexOf(destination), line2.stops.indexOf('Richmond')+1)
     }
-    var final_journey = line1_journey.concat(line2_journey.reverse());
-    console.log(`Stops: ${final_journey.length - 2}`);
-    console.log(final_journey.join(' ---> ').replace("Richmond ---> Richmond", 'Richmond || Richmond'));
+    multipleLineRoute(line1_journey, line2_journey);
 };
+
+function renderRoute(origin, destination, stops) {
+    document.querySelector('.origin').innerHTML = origin;
+    document.querySelector('.destination').innerHTML = destination;
+    document.querySelector('.stops').innerHTML = stops;
+};
+
+function getStops(journey) {
+    let finalJourneyArray = [];
+    for (let i = 0; i < journey.length; i++) {
+        finalJourneyArray.push(journey[i])
+    };
+    let route = finalJourneyArray.join('--->');
+    document.querySelector('.route').innerHTML = route;
+}
+
+function multipleLineRoute(line1_journey, line2_journey) {
+    let final_journey = line1_journey.concat(line2_journey.reverse());
+    let stops = final_journey.length - 2;
+    route = final_journey.join(' ---> ').replace("Richmond ---> Richmond", 'Richmond || Richmond');
+    document.querySelector('.route').innerHTML = route;
+    renderRoute(origin, destination, stops);
+}
